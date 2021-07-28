b4aae96a3aac9ee6069a0072c2056ef059d17b4d
Greyson-Moore <greysonmoore@my.unt.edu>
2021-07-27 20:27:18 -0500

    Interim commit

--- 06-Python-APIs/Instructions/starter_code/api_keys.py
+++ 06-Python-APIs/Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "e1b47485a6fc5eac68c9af5aaf31a80c"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBd9iGRQElGDhfkkqqhhJDMjpbYbr0w1MI"
