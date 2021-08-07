0a44e135302d985ee1f8d20e2ce4d21795d0a65e
Greyson Moore <85639051+Greyson-Moore@users.noreply.github.com>
2021-08-07 10:32:46 -0500

    Update api_keys.py

--- 06-Python-APIs/programs/api_keys.py
+++ 06-Python-APIs/programs/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "e1b47485a6fc5eac68c9af5aaf31a80c"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyBd9iGRQElGDhfkkqqhhJDMjpbYbr0w1MI"
+g_key = ""
